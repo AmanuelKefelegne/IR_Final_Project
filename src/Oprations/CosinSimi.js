dd8d6d428616c63b888cbcf07fa4ed4c7fefd72b
AmanuelKefelegne <demissieamanuel92@gmail.com>
2022-08-23 00:19:58 +0300

    done

--- src/Oprations/CosinSimi.js
+++ src/Oprations/CosinSimi.js
@@ -1,4 +1,32 @@
-function CosinSimi(){
-
+let inp=0;
+let inplist=[];
+let qul=0;
+let l=0;
+let listlen=[];
+let cosin=[];
+let c=0;
+function CosinSimi(termF,termFq,stemm,wordF,filenum){
+for(let j=0;j<filenum;j++){
+    for(let i=0;i<termF.length;i++){
+        inp+=termFq[i]*termF[i,j];
+    }
+    inplist+=inp;
+    inp=0;
+}
+for(let i=0;i<termFq.length;i++){
+    qul+=termFq[i]*termFq[i];
+}
+for(let i=0;i<filenum;i++){
+    for(let j=0;j<termF.length;j++){
+        l+=termF[j,i]*termF[j,i];
+    }
+    listlen+=l;
+    l=0;
+}
+for(let i=0;i<filenum;i++){
+ c=inplist[i]/Math.sqrt(qul*listlen[i])   
+}
+cosin+=c;
+c=0;
 }
 export default CosinSimi
--- src/Oprations/Query.js
+++ src/Oprations/Query.js
@@ -1,15 +1,69 @@
-import Stemmer from "./Stemmer"
+import Stemmer from './Operations/Stemmer';
 import DomF from "./DomF";
-import termW from "./File";
+import CosinSimi from "./Operations/CosinSimi"
 let word=[];
 let stemmq=[];
 let wordF;
+let stemm=[];
+let stem2=[];
+let termF=[];
+let termFq=[];
+let j=0;
 function Quary(query){
     word=query.split(" ");
     for(let i=0;i<word.length;i++){
         stemmq[i]=Stemmer(word[i])+" ";
     }
     wordF=DomF(stemmq);
+    File();
     termW(wordF);
+    CosinSimi(termF,termFq,stemm,wordF,filenum);
+}
+function File(){
+    for(let filenum=1;filenum<=3;filenum++){
+    fs.readFile('text'+filenum+'.txt','utf8',(err,data)=>{
+    if (err){
+        console.error(err);
+        return;
+    }
+     word=data.split(" " || "። " );
+    for(let i=0;i<word.length;i++){
+        stat=0;
+        stem2[i]=Stemmer(word[i])+" ";
+        stemm[j]+=stem2[i];
+        j++;
+    }
+    fs.writeFile('text1'+filenum+'.txt',stem2,err=>{
+        if(err){
+            console.err;
+            return;
+        }
+      });
+
+});}
+for(let k=0;k<=3;i++){
+fs.readFile('text1'+filenum+'.txt','utf8',(err,data)=>{
+    if (err){
+        console.error(err);
+        return;
+    }
+    stem2=data.split(" ");
+for(let i=0;i<stemm.length;i++){
+    for(let j=0;j<filenum;j++){
+        if(stemm[i]==stem2[j]){
+            termF[i,j]++;
+        }
+    }
+}
+});}
+}
+function termW(wordF){
+    let text=wordF.split(" ");
+    for(let i=0;i<text.length;i++){
+        if(stem2[i]==text[i]){
+            termFq[i]++;
+        }
+    }
+  
 }
 export default Quary
